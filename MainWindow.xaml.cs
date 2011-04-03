7a083ca66ae8f63d7731526e2eb375f50c36478f
astraway <astraway@localhost>
2011-04-03 12:51:51 +0400

    release

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System.Windows;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System;
+
+namespace WpfApplication
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Node> Nodes { get; private set; }
+
+        public MainWindow()
+        {
+            Nodes = new ObservableCollection<Node>();
+            InitializeComponent();
+            FillingTree();
+        }
+
+        /// <summary>
+        /// Filling tree different values
+        /// </summary>
+        private void FillingTree()
+        {
+            Nodes.Clear();
+            for (int i = 0; i < 5; i++)
+            {
+                var level_1_items = new Node() { Text = " Level 1 Item " + (i + 1) };
+                for (int j = 0; j < 2; j++)
+                {
+                    var level_2_items = new Node() { Text = " Level 2 Item " + (j + 1) };
+                    level_2_items.Parent.Add(level_1_items);
+                    level_1_items.Children.Add(level_2_items);
+                    for (int n = 0; n < 2; n++)
+                    {
+                        var level_3_items = new Node() { Text = " Level 3 Item " + (n + 1) };
+                        level_3_items.Parent.Add(level_2_items);
+                        level_2_items.Children.Add(level_3_items);
+                    }
+                }
+
+                Nodes.Add(level_1_items);
+            }
+            treeView.ItemsSource = Nodes;
+        }
+
+        /// <summary>
+        /// Take Id from CheckBox Uid and transfer value to CheckBoxId struct
+        /// </summary>
+        /// <param name="sender">The CheckBox clicked.</param>
+        /// <param name="e">Parameters associated to the mouse event.</param>
+        private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            CheckBox currentCheckBox = (CheckBox)sender;
+            CheckBoxId.checkBoxId = currentCheckBox.Uid;
+        }
+
+        #region Check, Uncheck and Invert all checkboxes
+
+        private void buttonCheckAll_Click(object sender, RoutedEventArgs e)
+        {
+            CheckedTree(Nodes, true);
+        }
+
+        private void buttonUncheckAll_Click(object sender, RoutedEventArgs e)
+        {
+            CheckedTree(Nodes, false);
+        }
+
+        private void CheckedTree(ObservableCollection<Node> items, bool isChecked)
+        {
+            foreach (Node item in items)
+            {
+                item.IsChecked = isChecked;
+                if (item.Children.Count != 0) CheckedTree(item.Children, isChecked);
+            }
+        }
+
+        private void buttonInvert_Click(object sender, RoutedEventArgs e)
+        {
+            CheckBoxId.checkBoxId = "";
+            InvertTree(Nodes);
+        }
+
+        private void InvertTree(ObservableCollection<Node> items)
+        {
+            foreach (Node item in items)
+            {
+                if (item.IsChecked != null)
+                {
+                    if (item.IsChecked == true) item.IsChecked = false;
+                    else item.IsChecked = true;
+                    if (item.Parent.Count!=0 && item.Parent[0].IsChecked == true) item.IsChecked = true;
+                    if (item.Parent.Count != 0 && item.Parent[0].IsChecked == false) item.IsChecked = false;
+                }
+                if (item.Children.Count != 0) InvertTree(item.Children);
+            }
+        }
+
+        #endregion
+
+        #region Expand and Collapse items
+
+        private void buttonExpand_Click(object sender, RoutedEventArgs e)
+        {
+            ExpandTree(Nodes, true);
+        }
+
+        private void buttonCollapse_Click(object sender, RoutedEventArgs e)
+        {
+            ExpandTree(Nodes, false);
+        }
+
+        private void ExpandTree(ObservableCollection<Node> items, bool isExpanded)
+        {
+            foreach (Node item in items)
+            {
+                item.IsExpanded = isExpanded;
+                if (item.Children.Count != 0) ExpandTree(item.Children, isExpanded);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Collections;
+using System;
+
+namespace WpfApplication
+{
+    public class Node : INotifyPropertyChanged
+    {
+        public Node()
+        {
+            this.id = Guid.NewGuid().ToString();
+        }
+
+        private ObservableCollection<Node> children = new ObservableCollection<Node>();
+        private ObservableCollection<Node> parent = new ObservableCollection<Node>();
+        private string text;
+        private string id;
+        private bool? isChecked = true;
+        private bool isExpanded;
+        
+        public ObservableCollection<Node> Children
+        {
+            get { return this.children; }
+        }
+
+        public ObservableCollection<Node> Parent
+        {
+            get { return this.parent; }
+        }
+
+        public bool? IsChecked
+        {
+            get { return this.isChecked; }
+            set
+            {
+                this.isChecked = value;
+                RaisePropertyChanged("IsChecked");
+            }
+        }
+
+        public string Text
+        {
+            get { return this.text; }
+            set
+            {
+                this.text = value;
+                RaisePropertyChanged("Text");
+            }
+        }
+
+        public bool IsExpanded
+        {
+            get { return isExpanded; }
+            set
+            {
+                isExpanded = value;
+                RaisePropertyChanged("IsExpanded");
+            }
+        }
+
+        public string Id
+        {
+            get { return this.id; }
+            set
+            {
+                this.id = value;
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            int countCheck = 0;
+            if (propertyName == "IsChecked")
+            {
+                if (this.Id == CheckBoxId.checkBoxId && this.Parent.Count == 0 && this.Children.Count != 0)
+                {
+                    CheckedTreeParent(this.Children, this.IsChecked);
+                }
+                if (this.Id == CheckBoxId.checkBoxId && this.Parent.Count > 0 && this.Children.Count > 0)
+                {
+                    CheckedTreeChildMiddle(this.Parent, this.Children, this.IsChecked);
+                }
+                if (this.Id == CheckBoxId.checkBoxId && this.Parent.Count > 0 && this.Children.Count == 0)
+                {
+                    CheckedTreeChild(this.Parent, countCheck);
+                }
+            }            
+        }
+
+        private void CheckedTreeChildMiddle(ObservableCollection<Node> itemsParent, ObservableCollection<Node> itemsChild, bool? isChecked)
+        {
+            int countCheck = 0;
+            CheckedTreeParent(itemsChild, isChecked);
+            CheckedTreeChild(itemsParent, countCheck);
+        }
+
+        private void CheckedTreeParent(ObservableCollection<Node> items, bool? isChecked)
+        {
+            foreach (Node item in items)
+            {
+                item.IsChecked = isChecked;
+                if (item.Children.Count != 0) CheckedTreeParent(item.Children, isChecked);
+            }
+        }
+
+        private void CheckedTreeChild(ObservableCollection<Node> items, int countCheck)
+        {            
+            bool isNull = false;
+            foreach (Node paren in items)
+            {
+                foreach (Node child in paren.Children)
+                {
+                    if (child.IsChecked == true || child.IsChecked == null)
+                    {
+                        countCheck++;
+                        if (child.IsChecked == null)
+                            isNull = true;
+                    }
+                }
+                if (countCheck != paren.Children.Count && countCheck != 0) paren.IsChecked = null;
+                else if (countCheck == 0) paren.IsChecked = false;
+                else if (countCheck == paren.Children.Count && isNull) paren.IsChecked = null;
+                else if (countCheck == paren.Children.Count && !isNull) paren.IsChecked = true;
+                if (paren.Parent.Count != 0) CheckedTreeChild(paren.Parent, 0);
+            }
+        }
+    }
+
+    public struct CheckBoxId
+    {
+        public static string checkBoxId;
+    }
+}
